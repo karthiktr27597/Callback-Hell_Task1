d6afcc4dc74904bd47733a46dbaf0e54b7e05e15
karthiktr27597 <karthiktr27597@gmail.com>
2023-03-26 20:17:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+   console.log(document.body.innerText = "10");
+   setTimeout(()=>{
+      console.log(document.body.innerText = "9");
+      setTimeout(()=>{
+        console.log(document.body.innerText = "8");
+        setTimeout(()=>{
+            console.log(document.body.innerText = "7");
+            setTimeout(()=>{
+                console.log(document.body.innerText = "6");
+                setTimeout(()=>{
+                    console.log(document.body.innerText = "5");
+                    setTimeout(()=>{
+                        console.log(document.body.innerText = "4");
+                        setTimeout(()=>{
+                            console.log(document.body.innerText = "3");
+                            setTimeout(()=>{
+                                console.log(document.body.innerText = "2");
+                                setTimeout(()=>{
+                                    console.log(document.body.innerText = "1");
+                                    setTimeout(()=>{
+                                        console.log(document.body.innerText = "Happy Independence Day :)");
+                                      },1000)
+                                  },1000)
+                              },1000)
+                          },1000)
+                      },1000)
+                  },1000)
+              },1000)
+          },1000)
+      },1000)
+   },1000)   
+},1000)
